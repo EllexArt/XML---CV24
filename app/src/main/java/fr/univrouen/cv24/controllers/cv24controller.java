b08fae824c6efdba3da77e3404e087655be50e9d
Etienne Binginot <etienne.binginot@gmail.com>
2024-04-10 13:32:15 +0200

    feature: add homepage route

--- app/src/main/java/fr/univrouen/cv24/controllers/cv24controller.java
+++ app/src/main/java/fr/univrouen/cv24/controllers/cv24controller.java
@@ -1,14 +1,20 @@
 package fr.univrouen.cv24.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.context.annotation.Description;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-class EmployeeController {
+class CV24Controller {
 
     @GetMapping("/")
+    @Operation(summary = "Homepage of the application")
+    @ResponseStatus(HttpStatus.OK)
     public String index() {
-        return "Hello cv24 !!";
+        return "Hello la team, Florian best bébou !!";
     }
 
 }
